2eef63f72b5cc5ef33522599f62c68ea3debe1a8
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-07-05 18:28:33 +0300

    Update types.go

--- store/types.go
+++ store/types.go
@@ -42,13 +42,13 @@ const (
 
 var (
 	// custom errors
-	ErrNotFound               = errors.New("not found")
-	ErrUnsupported            = errors.New("not supported")
-	ErrEpochPivotSwitched     = errors.New("epoch pivot switched")
-	ErrContinousEpochRequired = errors.New("continous epoch required")
-	ErrAlreadyPruned          = errors.New("data already pruned")
-	ErrChainReorged           = errors.New("chain re-orged")
-	ErrLeaderRenewal          = errors.New("leadership renewal failure")
+	ErrNotFound                = errors.New("not found")
+	ErrUnsupported             = errors.New("not supported")
+	ErrEpochPivotSwitched      = errors.New("epoch pivot switched")
+	ErrContinuousEpochRequired = errors.New("continuous epoch required")
+	ErrAlreadyPruned           = errors.New("data already pruned")
+	ErrChainReorged            = errors.New("chain re-orged")
+	ErrLeaderRenewal           = errors.New("leadership renewal failure")
 
 	// operationable epoch data types
 	OpEpochDataTypes = []EpochDataType{
